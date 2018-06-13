3c30ee2ddceddbf0df8822b1f4b4f64a8fd77723
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2018-06-13 18:47:29 +0200

    sample implementation of the Python SDK

--- webhook.py
+++ webhook.py
@@ -8,6 +8,9 @@ from datetime import timedelta
 from os import listdir
 from os.path import isfile, join
 from twilio.rest import Client
+import oneagent.sdk 
+# Singleton for the OneAgent
+from oneagent.common import AgentState
 import getpass
 import sys
 import os
@@ -19,6 +22,7 @@ import logging
 import subprocess
 import requests
 import traceback
+from bottle import response
 
 #######################
 # Dynatrace Webhook API Custom Integration 
@@ -28,6 +32,15 @@ import traceback
 # it also sends an SMS and finally posts the results
 # in the problem comments in Dynatrace for collaboration.
 #######################
+
+# Check OneAgent SDK initialization 
+sdk = oneagent.sdk.SDK.get()
+if sdk.agent_state == AgentState.ACTIVE:
+    print('OneAgent is Active and hooked into this process.')
+elif sdk.agent_state == AgentState.TEMPORARILY_INACTIVE:
+    print('OneAgent is temporarily inactive but could be hooked later into this process.')
+else:
+    print('OneAgent is inactive or not available. No tracing active for this process.')
 
 # Uptime variable
 start_time = timeit.default_timer()
@@ -87,6 +100,7 @@ def check_create_dir(dir_name):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
+
 # Logging configuration
 # Create log directory at initialization
 check_create_dir(LOGDIR)
@@ -98,6 +112,11 @@ logging.getLogger().addHandler(logging.StreamHandler())
 # Set Twilio logging to warning. 
 logging.getLogger("twilio").setLevel(logging.WARNING)
 
+# Create Application for the OneAgent
+sdkapp = sdk.create_web_application_info(
+    virtual_host="https://{0}:{1}".format(WEBHOOK_INTERFACE, WEBHOOK_PORT),
+    application_id='Custom Python Webhook',
+    context_root='/')
 
 # Initiate Flask Microservice with basic authentication
 app = Flask(__name__)
@@ -109,25 +128,41 @@ app.config['BASIC_AUTH_PASSWORD'] = PASSWORD
 # app.config['BASIC_AUTH_FORCE'] = True
 basic_auth = BasicAuth(app)
 
+
 # Flask listener for POST Method with authorization
 @app.route('/', methods=['POST'])
 @basic_auth.required
 def handle_post():
     try:
-        global prob_count 
-        prob_count += 1
-        problem_simple = json.loads(request.data)
-        logging.info('Notification received from ' + request.remote_addr);
-        
-        # JSON Payload will be saved in a directory
-        save_request(problem_simple)
-        if "999" in problem_simple['ProblemID']:
-            logging.info('Test message successfully received. No integration will be called')
-            return "OK"
-        
-        # Integrations will be called
-        call_integration(problem_simple['PID'])
-        
+        h = request.headers
+        wreq = sdk.trace_incoming_web_request(sdkapp, request.full_path, request.method, headers={'Connection':h.get('Connection'), 'Accept':h.get('Accept'), 'Host': h.get('Host'), 'Cache-Control':h.get('Cache-Control')},
+                                               remote_address=request.remote_addr)
+        with wreq:
+            wreq.add_parameter('user-agent', str(request.user_agent))
+            wreq.add_parameter('Client IP', str(request.remote_addr))
+            global prob_count 
+            prob_count += 1
+            problem_simple = json.loads(request.data)
+            logging.info('Notification received from ' + request.remote_addr);
+            
+            # JSON Payload will be saved in a directory
+            save_request(problem_simple)
+            
+            # show the problem Id in Purepath
+            wreq.add_parameter('ProblemID', problem_simple['ProblemID'])
+            
+            # If Test Msg do not call integration
+            if "999" in problem_simple['ProblemID']:
+                logging.info('Test message successfully received. No integration will be called')
+                wreq.add_response_headers(response.headers)
+                wreq.set_status_code(response.status_code)
+                return "OK"
+            
+            # Integrations will be called
+            call_integration(problem_simple['PID'])
+            
+            wreq.add_response_headers(response.headers)
+            wreq.set_status_code(response.status_code)
     except Exception as e:
         logging.error("There was an error handling the Request")
         logging.error(traceback.format_exc())
@@ -242,44 +277,57 @@ def get_buttons_from_relativetimes():
 # with public access
 @app.route('/', methods=['GET'])
 def handle_get():
-    time_option = request.args.get('relativeTime')
     
-    flash(Markup("<br>Python Flask Webhook endpoint: " + TENANT_HOST + "</br>"))
-    flash(Markup("<br>Flask Web Microservice running on: https://{0}:{1}".format(WEBHOOK_INTERFACE, WEBHOOK_PORT)))
-    flash(Markup("<br>Received notifications: {0}".format(prob_count)))
-    flash(Markup("<br>Path: {0}".format(os.getcwd())))
-    flash(Markup("<br>Host: {0}".format(socket.gethostname())))
-    flash(Markup("<br>User: {0}".format(WEBHOOK_USERNAME)))
-    flash(Markup("<br>PID: {0}".format(os.getpid())))
-    flash(Markup("<br>Uptime: {0}".format(get_uptime())))
-    # TODO JQuery efect
-    
-    flash(Markup("<br><button onclick=\"showHideById('usage')\">toggle usage</button>"))
-    flash(Markup("<div id=\"usage\" class=\"usage\">"))
-    flash(Markup("{0}".format(get_usage_as_html())))
-    flash(Markup("</div>"))
-    flash(Markup("<br><br>Poll the problems via API for the last:"))
-    flash(Markup(get_buttons_from_relativetimes()))
-        
-    if time_option:
-        data = get_problemsfeed_by_time(time_option)
-        flash(Markup("<br><button onclick=\"showHideById('table_poll')\">toggle poll table</button>"))
-        flash(Markup("<div id=\"table_poll\">"))
-        flash(Markup("<br>There were {0} problems found during the selected timeframe '{1}'".format(
-                len(data['result']['problems']), time_option)))
-        flash(Markup("<br>Dynatrace has monitored the following entities in the last '{0}':".format(time_option)))
-        flash(Markup("<br>APPLICATION:\t {:6}".format(data['result']['monitored']['APPLICATION'])))
-        flash(Markup("<br>SERVICE:\t {:6}".format(data['result']['monitored']['SERVICE'])))
-        flash(Markup("<br>INFRASTRUCTURE:\t {:6}".format(data['result']['monitored']['INFRASTRUCTURE'])))
-        flash(Markup(get_table_from_list(data['result']['problems'])))
+    h = request.headers
+    wreq = sdk.trace_incoming_web_request(sdkapp, request.full_path, request.method, headers={'Connection':h.get('Connection'), 'Accept':h.get('Accept'), 'Host': h.get('Host'), 'Cache-Control':h.get('Cache-Control')},
+    remote_address=request.remote_addr)
+
+    with wreq:
+        wreq.add_parameter('user-agent', str(request.user_agent))
+        wreq.add_parameter('Client IP', str(request.remote_addr))
+        # Process web request
+        time_option = request.args.get('relativeTime')
+        flash(Markup("<br>Python Flask Webhook endpoint: " + TENANT_HOST + "</br>"))
+        flash(Markup("<br>Flask Web Microservice running on: https://{0}:{1}".format(WEBHOOK_INTERFACE, WEBHOOK_PORT)))
+        flash(Markup("<br>Received notifications: {0}".format(prob_count)))
+        flash(Markup("<br>Path: {0}".format(os.getcwd())))
+        flash(Markup("<br>Host: {0}".format(socket.gethostname())))
+        flash(Markup("<br>User: {0}".format(WEBHOOK_USERNAME)))
+        flash(Markup("<br>PID: {0}".format(os.getpid())))
+        flash(Markup("<br>Uptime: {0}".format(get_uptime())))
+        # TODO JQuery efect
+        flash(Markup("<br><button onclick=\"showHideById('usage')\">toggle usage</button>"))
+        flash(Markup("<div id=\"usage\" class=\"usage\">"))
+        flash(Markup("{0}".format(get_usage_as_html())))
+        flash(Markup("</div>"))
+        flash(Markup("<br><br>Poll the problems via API for the last:"))
+        flash(Markup(get_buttons_from_relativetimes()))
+            
+        if time_option:
+            data = get_problemsfeed_by_time(time_option)
+            flash(Markup("<br><button onclick=\"showHideById('table_poll')\">toggle poll table</button>"))
+            flash(Markup("<div id=\"table_poll\">"))
+            flash(Markup("<br>There were {0} problems found during the selected timeframe '{1}'".format(
+                    len(data['result']['problems']), time_option)))
+            flash(Markup("<br>Dynatrace has monitored the following entities in the last '{0}':".format(time_option)))
+            flash(Markup("<br>APPLICATION:\t {:6}".format(data['result']['monitored']['APPLICATION'])))
+            flash(Markup("<br>SERVICE:\t {:6}".format(data['result']['monitored']['SERVICE'])))
+            flash(Markup("<br>INFRASTRUCTURE:\t {:6}".format(data['result']['monitored']['INFRASTRUCTURE'])))
+            flash(Markup(get_table_from_list(data['result']['problems'])))
+            flash(Markup("</div>"))
+            
+        flash(Markup("<br><br><button onclick=\"showHideById('table_saved')\">toggle sent table</button>"))
+        flash(Markup("<div id=\"table_saved\">"))
+        flash(Markup("Successfully sent problems (saved in {0}\{1}):".format(os.getcwd(), DIR_SENT)))
+        flash(Markup("<br>" + get_table()))
         flash(Markup("</div>"))
         
-    flash(Markup("<br><br><button onclick=\"showHideById('table_saved')\">toggle sent table</button>"))
-    flash(Markup("<div id=\"table_saved\">"))
-    flash(Markup("Successfully sent problems (saved in {0}\{1}):".format(os.getcwd(), DIR_SENT)))
-    flash(Markup("<br>" + get_table()))
-    flash(Markup("</div>"))
-    return render_template('index.html')
+        template = render_template('index.html')
+        
+        wreq.add_response_headers(response.headers)
+        wreq.set_status_code(response.status_code)
+    
+    return template
 
 
 def get_usage_as_html():
@@ -307,25 +355,33 @@ def getAuthenticationHeader():
 
 
 def get_problemsfeed_by_time(time_option):
-    msg = "fetching prob_count for '" + time_option + "' - " + API_ENDPOINT_PROBLEM_FEED
-    logging.info(msg)
-    response = requests.get(TENANT_HOST + API_ENDPOINT_PROBLEM_FEED + "?relativeTime=" + time_option,
-                            headers=getAuthenticationHeader(), verify=verifyRequest())
-
-    handle_response_status(msg, response)
-    data = json.loads(response.text)
-    logging.debug('Reponse content: ' + str(response.content))
+    outcall = sdk.trace_outgoing_remote_call(
+    'get_problemsfeed_by_time', 'Dynatrace API', API_ENDPOINT_PROBLEM_FEED + "?relativeTime=" + time_option, oneagent.sdk.Channel(oneagent.sdk.ChannelType.TCP_IP, TENANT_HOST),
+    protocol_name='HTTP/custom')
+    with outcall:
+        msg = "fetching prob_count for '" + time_option + "' - " + API_ENDPOINT_PROBLEM_FEED
+        logging.info(msg)
+        response = requests.get(TENANT_HOST + API_ENDPOINT_PROBLEM_FEED + "?relativeTime=" + time_option,
+                                    headers=getAuthenticationHeader(), verify=verifyRequest())
+        
+        handle_response_status(msg, response)
+        data = json.loads(response.text)
+        logging.debug('Response content: ' + str(response.content)) 
     return data
 
 
 def get_problem_by_id(problemid):
-    msg = "fetching problem id " + str(problemid)
-    logging.info(msg)
-    response = requests.get(TENANT_HOST + API_ENDPOINT_PROBLEM_DETAILS + problemid, headers=getAuthenticationHeader(),
+    outcall = sdk.trace_outgoing_remote_call(
+    'get_problem_by_id', 'Dynatrace API', API_ENDPOINT_PROBLEM_DETAILS + problemid, oneagent.sdk.Channel(oneagent.sdk.ChannelType.TCP_IP, TENANT_HOST),
+    protocol_name='HTTP/custom')
+    with outcall:
+        msg = "fetching problem id " + str(problemid)
+        logging.info(msg)
+        response = requests.get(TENANT_HOST + API_ENDPOINT_PROBLEM_DETAILS + problemid, headers=getAuthenticationHeader(),
                             verify=verifyRequest())
-    handle_response_status(msg, response)
-    data = json.loads(response.text)
-    logging.info("Problem ID " + problemid + " fetched")
+        handle_response_status(msg, response)
+        data = json.loads(response.text)
+        logging.info("Problem ID " + problemid + " fetched")
     return data['result']
 
 
@@ -386,7 +442,6 @@ def post_incident_result_in_problem_comments(problem, return_code, error):
     data['user'] = WEBHOOK_USERNAME
     data['context'] = 'Incident Software Custom Integration'
     
-    
     r = post_in_comments(problem, data);
 
     if r.status_code == 200:
@@ -400,13 +455,18 @@ def post_incident_result_in_problem_comments(problem, return_code, error):
                                                                                                           data))
     return
 
+
 def post_in_comments(problem, data):
-    # Define header
-    headers = {'content-type': 'application/json', "Authorization": "Api-Token " + API_TOKEN}
-    # Make POST Request
-    r = requests.post(TENANT_HOST + API_ENDPOINT_PROBLEM_DETAILS + problem['id'] + "/comments", json=data,
-                      headers=headers, verify=verifyRequest())
-    # Return response
+    outcall = sdk.trace_outgoing_remote_call(
+    'post_in_comments', 'Dynatrace API', API_ENDPOINT_PROBLEM_DETAILS + problem['id'] + "/comments", oneagent.sdk.Channel(oneagent.sdk.ChannelType.TCP_IP, TENANT_HOST),
+    protocol_name='HTTP/custom')
+    with outcall:
+        # Define header
+        headers = {'content-type': 'application/json', "Authorization": "Api-Token " + API_TOKEN}
+        # Make POST Request
+        r = requests.post(TENANT_HOST + API_ENDPOINT_PROBLEM_DETAILS + problem['id'] + "/comments", json=data,
+                          headers=headers, verify=verifyRequest())
+        # Return response
     return r
 
 
@@ -417,7 +477,7 @@ def call_integration(problem_id):
     problem_details = get_problem_by_id(problem_id)
         
     # Notify the incident software and comment the result in Dynatrace
-    if INCIDENT_NOTIFICATION: 
+    if INCIDENT_NOTIFICATION:
         call_incident_software(problem_details)
     
     # Send an SMS message and comment in Dynatrace
@@ -434,63 +494,72 @@ def call_integration(problem_id):
 
 
 def call_sms_integration(problem_details):
-    
-    # SMS Client initialized with the Twilio Account (SID and Auth-Token)
-    sms_client = Client(TWILIO_ACCOUNT, TWILIO_TOKEN)
-    
-    level = problem_details["impactLevel"]
-    nr =  problem_details["displayName"]
-    pid = problem_details["id"]
-    status = problem_details["status"]
-    
-    # change the "from_" number to your Twilio number and the "to" number
-    # to the phone number you signed up for Twilio with, or upgrade your
-    # account to send SMS to any phone number
-    body = "Dynatrace notification - {0} problem ({1}) {5}. Open in Dynatrace:{2}{3}{4}".format(level.lower(), nr, TENANT_HOST, UI_PROBLEM_DETAIL_BY_ID, pid, status.lower())
-    sms_client.messages.create(to=TO_NUMBER, from_=TWILIO_NUMBER, body=body)
-     
-    TO_NUMBER
-    # Post SMS result in the comments
-    data = {}
-    data['comment'] = "Mobile number has been notified: {0}".format(anonymize_numer(TO_NUMBER))
-    data['user'] = WEBHOOK_USERNAME
-    data['context'] = 'Twilio Custom Integration'
-    r = post_in_comments(problem_details, data)
-    # Log to the console
-    logging.info('{0}: {1} sent from {2}'.format(data['context'] , data['comment'], TWILIO_NUMBER))
+    outcall = sdk.trace_outgoing_remote_call(
+    'call_sms_integration', 'TwilioService', 'Twilio API' , oneagent.sdk.Channel(oneagent.sdk.ChannelType.TCP_IP, TWILIO_NUMBER),
+    protocol_name='SMS/custom')
+    with outcall:
+        # SMS Client initialized with the Twilio Account (SID and Auth-Token)
+        sms_client = Client(TWILIO_ACCOUNT, TWILIO_TOKEN)
+        
+        level = problem_details["impactLevel"]
+        nr = problem_details["displayName"]
+        pid = problem_details["id"]
+        status = problem_details["status"]
+        
+        # change the "from_" number to your Twilio number and the "to" number
+        # to the phone number you signed up for Twilio with, or upgrade your
+        # account to send SMS to any phone number
+        body = "Dynatrace notification - {0} problem ({1}) {5}. Open in Dynatrace:{2}{3}{4}".format(level.lower(), nr, TENANT_HOST, UI_PROBLEM_DETAIL_BY_ID, pid, status.lower())
+        sms_client.messages.create(to=TO_NUMBER, from_=TWILIO_NUMBER, body=body)
+         
+        TO_NUMBER
+        # Post SMS result in the comments
+        data = {}
+        data['comment'] = "Mobile number has been notified: {0}".format(anonymize_numer(TO_NUMBER))
+        data['user'] = WEBHOOK_USERNAME
+        data['context'] = 'Twilio Custom Integration'
+        r = post_in_comments(problem_details, data)
+        # Log to the console
+        logging.info('{0}: {1} sent from {2}'.format(data['context'] , data['comment'], TWILIO_NUMBER))
     return
+
 
 def anonymize_numer(number):
     return str(number[0:3] + '*****' + number[-4:])
 
+
 def call_incident_software(problem_details):
-    
     problem_nr = problem_details['displayName']
     # Check the OS of the program to call (Windows or Linux)
     if os.name == 'nt':
         EXECUTABLE = EXEC_WIN
     else:
         EXECUTABLE = EXEC_UNIX
-
-    argument_list = get_program_argument(problem_details)
     
-    return_codes = []
-    for argument in argument_list:
-        return_code = (subprocess.call(EXECUTABLE + ' ' + argument, shell=True))
-        logging.info('Incident Software call for [{0}] RC[{1}] Executable:[{2}] Arguments:{3}'.format(str(problem_nr), return_code, EXECUTABLE, argument))
-        return_codes.append(return_code)
+    outcall = sdk.trace_outgoing_remote_call(
+    'call_incident_software', 'LegacyService', os.getcwd() , oneagent.sdk.Channel(oneagent.sdk.ChannelType.TCP_IP, EXECUTABLE),
+    protocol_name='CLI/custom')
 
-    # Check all the return codes
-    for r in return_codes:
-        r += r
-    # If the sum is bigger than zero, a problem occurred when calling the Incident software.
-    if r == 0:
-        logging.info('All calls to the Incident Software for [{0}] OK, Return Codes{1}'.format(problem_nr, return_codes))
-        post_incident_result_in_problem_comments(problem_details, return_codes, False)
+    with outcall:
+        argument_list = get_program_argument(problem_details)
         
-    else:
-        logging.error('There was a problem calling the Incident Software [{0}], Return Codes{1}'.format(problem_nr, return_codes))
-        post_incident_result_in_problem_comments(problem_details, return_codes, True)
+        return_codes = []
+        for argument in argument_list:
+            return_code = (subprocess.call(EXECUTABLE + ' ' + argument, shell=True))
+            logging.info('Incident Software call for [{0}] RC[{1}] Executable:[{2}] Arguments:{3}'.format(str(problem_nr), return_code, EXECUTABLE, argument))
+            return_codes.append(return_code)
+    
+        # Check all the return codes
+        for r in return_codes:
+            r += r
+        # If the sum is bigger than zero, a problem occurred when calling the Incident software.
+        if r == 0:
+            logging.info('All calls to the Incident Software for [{0}] OK, Return Codes{1}'.format(problem_nr, return_codes))
+            post_incident_result_in_problem_comments(problem_details, return_codes, False)
+            
+        else:
+            logging.error('There was a problem calling the Incident Software [{0}], Return Codes{1}'.format(problem_nr, return_codes))
+            post_incident_result_in_problem_comments(problem_details, return_codes, True)
     return
 
 
